f986fb530ebe48c73b70f37b14f662edc6ed46c5
thanhphongdang <thanhphong25@gmail.com>
2025-04-04 08:47:12 +0700

    create file EXE_LARAVEL

---
+++ EXE/resources/views/EXE1/create.blade.php
@@ -0,0 +1,32 @@
+@extends('dashboard')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <h3>Create User</h3>
+                    <form action="{{ route('user.postUser') }}" method="POST">
+                        @csrf
+                        <div class="form-group mb-3">
+                            <input type="text" placeholder="Name" id="name" class="form-control" name="name" required autofocus>
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="email" placeholder="Email" id="email" class="form-control" name="email" required autofocus>
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="phone" placeholder="Phone" id="phone" class="form-control" name="phone" required
+                                autofocus>
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="password" placeholder="Password" id="password" class="form-control" name="password" required
+                                autofocus>
+                        </div>
+                        <div class="d-grid mx-auto">
+                           <button type="submit" class="btn btn-dark btn-block">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
